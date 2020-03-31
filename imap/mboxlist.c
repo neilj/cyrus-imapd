df329f382c614fad91878f74697abfee602bc79e
Ken Murchison <murch@fastmail.com>
2020-03-31 13:34:57 -0400

    mboxlist.c: create new mailboxes using the same directory scheme (by name or by uuid) as its parent

--- imap/mboxlist.c
+++ imap/mboxlist.c
@@ -1625,7 +1625,7 @@ static int mboxlist_createmailbox_full(const char *mboxname, int mbtype,
     char *acl = NULL;
     struct mailbox *newmailbox = NULL;
     int isremote = mbtype & MBTYPE_REMOTE;
-    mbentry_t *newmbentry = NULL;
+    mbentry_t *parent = NULL, *newmbentry = NULL;
 
     r = mboxlist_create_namecheck(mboxname, userid, auth_state,
                                   isadmin, forceuser);
@@ -1643,6 +1643,12 @@ static int mboxlist_createmailbox_full(const char *mboxname, int mbtype,
 
     r = mboxlist_create_partition(mboxname, partition, &newpartition);
     if (r) goto done;
+
+    r = mboxlist_findparent(mboxname, &parent);
+    if (!r) {
+        mbtype |= (parent->mbtype & MBTYPE_LEGACY_DIRS);
+    }
+    else if (r != IMAP_MAILBOX_NONEXISTENT) goto done;
 
     if (!dbonly && !isremote) {
         /* Filesystem Operations */
@@ -1704,6 +1710,7 @@ done:
     free(acl);
     free(newpartition);
     mboxlist_entry_free(&newmbentry);
+    mboxlist_entry_free(&parent);
 
     return r;
 }
